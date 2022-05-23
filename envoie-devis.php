c69bb5e2ba7f73096ffe2fe1e15f41c7a7c555bc
cmoynard <cmoynard.etude@gmail.com>
2022-05-23 23:41:46 +0200

    first commit

---
+++ envoie-devis.php
@@ -0,0 +1,8 @@
+<?php
+
+echo '<script>
+        alert("Le devis à été envoyé !");
+        location.href="index.php";
+        </script>';
+
+?>
---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>LJ Consulting</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Mako&display=swap" rel="stylesheet">
+        
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        
+        <style>
+            .bg-cover {
+                background-size: cover !important;
+            }
+
+            .iconlink, .iconlink:visited {
+                color: white !important;
+            }
+        </style>
+    </head>
+
+    
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-scroll navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand mb-0 h1" href="index.php">
+                    <img src="images/logo final.jpg" alt="Logo de LJ Consulting" width="50" height="50">
+                LJ Consulting RH
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
+                        </li>
+    
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Nos solutions
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">PME</a></li>
+                                <li><a class="dropdown-item" href="#">Grandes entreprises</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">A l'étranger</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+
+        <img src="images/wide.jpg" alt="banner" class="img-fluid w-100">
+    </header>
+
+    <body>
+        <section>
+        <div class="container-fluid">
+            <div class="row" style="height: 400px;">
+                <div class="col-md-6 text-light" style="background-color:grey;">
+                    <div class="row align-items-center text-center">
+                        <h2 class="mt-5"><strong>Qui sommes-nous ?</strong></h2>
+                        <div class="col-md-2"></div>
+                        <div class="col-md-8">
+                        <p class="mt-4 text-center" style="font-size: 1.1em;">LJ Consulting RH c’est une équipe de consultants indépendants, experts dans leurs domaines. Chaque jour, nous sommes à vos cotés pour vous accompagner à chaque étapes de vos besoins en ressources humaines.</p>
+                        </div>
+                        <div class="col-md-2"></div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row align-items-center text-center">
+                        <div class="container-fluid">
+                            <img class="mt-5 border border-dark" src="images/imagelinda.jpg" alt="Image de Madame">
+                        </div>
+                        <h4 class="mt-3"><strong>Linda PINTO</strong></h4>
+                        <p class="mt-1">Fondatrice de LJ Consulting RH</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+
+
+        <section>
+        <div class="container-fluid border-top border-dark">
+            <div class="row align-items-center" style="height: 400px;">
+            
+                <div class="col-md-4 text-dark">
+                    <div class="row align-items-center text-center">
+                        <div class="col-md-2"></div>
+                        <h1 class=" col-md-8 mx-2 mt-4"><i class="uil uil-wallet"></i></h1>
+                        <div class="col-md-2"></div>
+
+                        <h2 class="mt-3 text-center"><strong>Diminuer vos coûts</strong></h2>
+                        <div class="col-md-2"></div>
+                        <div class="col-md-8">
+                            <p class="mt-2 text-center">Économisez vos coûts de RH en faisant appel à une spécialiste pour vous concentrer sur votre cœur de métier !</p>
+                        </div>
+                        <div class="col-md-2"></div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 text-dark">
+                    <div class="row align-items-center text-center">
+                        <div class="col-md-2"></div>
+                        <h1 class=" col-md-8 mx-2 mt-4"><i class="uil uil-chart-growth"></i></h1>
+                        <div class="col-md-2"></div>
+
+                        <h2 class="mt-3 text-center"><strong>Des résultats éprouvés</strong></h2>
+                        <div class="col-md-2"></div>
+                        <div class="col-md-8">
+                            <p class="mt-2 text-center">Depuis 2018, plus de 60 recrutements à notre actif dans les secteurs : Agroalimentaires, Services, Industriel, IT, Médical.
+                                Plus de 30 entreprises nous font confiance.</p>
+                        </div>
+                        <div class="col-md-2"></div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 text-dark">
+                    <div class="row align-items-center text-center">
+                        <div class="col-md-2"></div>
+                        <h1 class=" col-md-8 mx-2 mt-4"><i class="uil uil-check-square"></i></h1>
+                        <div class="col-md-2"></div>
+
+                        <h2 class="mt-3 text-center"><strong>Expérience et expertise</strong></h2>
+                        <div class="col-md-2"></div>
+                        <div class="col-md-8">
+                            <p class="mt-2 text-center">Nos consultants sont des experts dans leur domaines en ressources humaines et en droit social. Notre méthode s’appuie sur une logique pragmatique :
+                                Analyse, action, résultats.</p>
+                        </div>
+                        <div class="col-md-2"></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        </section>
+
+        <section>
+            <div class="container-fluid border-top border-dark">
+                <div class="row align-items-center" style="height: 600px;">
+                    <div class="col-md-4">
+                        <h3 class="mx-5 text-center">Coordonnées</h3>
+                        <h4 class="mx-4 my-4"><i class="uil uil-phone-alt"> 07 81 95 22 00</i></h4>
+                        <h4 class="mx-4 my-4"><i class="uil uil-location-pin-alt"> 14 rue A. Beau de Rochas, 66330 Cabestany</i></h4>
+                        <h4 class="mx-4 my-4"><i class="uil uil-at"> contactentreprise@ljconsultingrh.com</i></h4>
+                    </div>
+
+                    <div class="col-md-8">
+                        <h3 class="mx-5 text-center">Obtenez un devis gratuit :</h3>
+                        <div class="row">
+                            <div class="col-md-2"></div>
+                            <div class="col-md-8">
+                            <form action="envoie-devis.php" id="form" method="POST" novalidate>
+                                <div class="form-outline">
+                                <input type="text" id="societ" name="societ" placeholder="Société" pattern="[a-zA-ZàâæçéèêëîïôœùûüÿÀÂÆÇnÉÈÊËÎÏÔŒÙÛÜŸ-]+" minlength="3" maxlength="40" class="mt-4 form-control" required>
+                                <div class="valid-feedback">
+                                    Nom de société OK !
+                                </div>
+                                <div class="invalid-feedback">
+                                    Veuillez entrer un nom valide.
+                                </div>
+                                </div>
+
+                                <div class="form-outline">
+                                <input type="text" id="nom" name="nom" placeholder="Nom" pattern="[a-zA-ZàâæçéèêëîïôœùûüÿÀÂÆÇnÉÈÊËÎÏÔŒÙÛÜŸ-]+" minlength="3" maxlength="40" class="mt-4 form-control" required>
+                                <div class="valid-feedback">
+                                    Nom OK !
+                                </div>
+                                <div class="invalid-feedback">
+                                    Veuillez entrer un nom valide.
+                                </div>
+                                </div>
+
+                                <div class="form-outline">
+                                <input type="" id="num" name="num" placeholder="Numéro de téléphone" pattern="[0-9]{10}" class="mt-4 form-control" required>
+                                <div class="valid-feedback">
+                                    Numéro OK !
+                                </div>
+                                <div class="invalid-feedback">
+                                    Veuillez entrer un numéro valide.
+                                </div>
+                                </div>
+
+                                <div class="form-outline">
+                                <input type="text" id="mail" name="mail" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" class="mt-4 form-control" required>
+                                <div class="valid-feedback">
+                                    Email OK !
+                                </div>
+                                <div class="invalid-feedback">
+                                    Veuillez entrer un email valide.
+                                </div>
+                                </div>
+
+                                <button type="submit" class="btn btn-outline-success mt-4">Envoyer le devis</button>
+                            
+                            </form>
+                            </div>
+                            <div class="col-md-2"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+
+    <footer class="text-center text-light">
+        <div class="container-fluid" style="background-color: #3E4145;">
+            <div style="margin:0 auto"></div>
+            <div class="row mt-3">
+                <div class="col-md-2"></div>
+                <div class="col-md-3 mb-2 d-flex">
+                    <h2>LJ Consulting</h2>
+                    <h6 class="mx-3 mt-4">Votre copilote dans toutes vos décisions</h6>
+                </div>
+                <div class="col-md-4"></div>
+                <div class="col-md-2 mt-4 d-flex">
+                    <h5 class="mx-2"><a href="https://www.facebook.com/ConsultingRH66/" class="iconlink" ><i class="uil uil-facebook-f"></i></a></h5>
+                    <h5 class="mx-2"><a href="https://twitter.com/ljconsult66" class="iconlink" ><i class="uil uil-twitter-alt"></i></a></h5>
+                    <h5 class="mx-2"><a href="https://www.linkedin.com/company/lj-consulting-rh/?originalSubdomain=fr" class="iconlink"><i class="uil uil-linkedin-alt"></i></a></h5>
+                </div>
+            </div>
+        </div>
+    </footer>    
+
+
+
+
+    <script>
+
+
+        var mail=document.getElementById("mail");
+        mail.addEventListener("blur", function (evt) {
+            console.log("Perte de focus pour le mail");
+        });
+
+        var societe=document.getElementById("societ");
+        societe.addEventListener("blur", function (evt) {
+            console.log("Perte de focus pour le nom de société");
+        });
+
+        var nom=document.getElementById("nom");
+        nom.addEventListener("blur", function (evt) {
+            console.log("Perte de focus pour le nom");
+        });
+
+        var num=document.getElementById("num");
+        num.addEventListener("blur", function (evt) {
+            console.log("Perte de focus pour le numéro");
+        });
+
+        (function() {
+        "use strict"
+        window.addEventListener("load", function() {
+            var form = document.getElementById("form")
+            form.addEventListener("submit", function(event) {
+            if (form.checkValidity() == false) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            form.classList.add("was-validated")
+            }, false)
+        }, false)
+
+        }())
+    </script>
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    </body>
+</html>
